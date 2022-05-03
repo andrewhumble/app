6b85d63fd363d17c1a780b52115814bbb025214b
nishar0119 <nr59672@uga.edu>
2022-05-03 00:50:45 -0400

    view

--- view.php
+++ view.php
@@ -4,18 +4,18 @@
     //connect to database
     require_once('connDB.php');
     // Check connection
-    if ($conn === false) {
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
-    $userType = $_SESSION['userType'];
-    //ensures someone is logged inbefore allowing them to create a profile
-    if (!isset($_SESSION['username']) || $_SESSION['userType'] != 3) {
+     if ($conn === false) {
+         die("ERROR: Could not connect. " . mysqli_connect_error());
+     }
+     $userType = $_SESSION['userType'];
+    // //ensures someone is logged inbefore allowing them to create a profile
+     if (!isset($_SESSION['username']) || $_SESSION['userType'] != 3) {
     
-        header("Location: welcome.html");
-        exit();
-    } else {
-        $username = $_SESSION['username'];
-        $userType = $_SESSION['userType'];
+         header("Location: welcome.html");
+         exit();
+     } else {
+         $username = $_SESSION['username'];
+         $userType = $_SESSION['userType'];
     
         
     }
@@ -121,10 +121,12 @@
 
     <?php
 
+       
+
     if (isset($_POST["submit"])) {
-        $ISBN = $_POST["Search"];
-        $val = $conn->query("SELECT imgPath FROM `book` WHERE ISBN ='$ISBN'");
-        $res = $conn->query("SELECT * FROM `book` WHERE ISBN = '$ISBN'");
+        $_SESSION['is'] = $_POST["Search"];
+        $val = $conn->query("SELECT image FROM `book` WHERE isbn= '".$_SESSION['is']."'");
+        $res = $conn->query("SELECT * FROM `book` WHERE isbn = '".$_SESSION['is']."'" );
         $results = mysqli_num_rows($res);
         if ($results > 0) {
             while ($row = mysqli_fetch_object($res)) {
@@ -136,18 +138,18 @@
                     <div class="center">
 
                         <!-- Uploading image -->
-                        <?php if ($val->num_rows > 0) { ?>
+                        <?php if ( $val->num_rows > 0) { ?>
                             <?php while ($blah = $val->fetch_assoc()) { ?>
-                                <img id="pic" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($blah['imgPath']); ?>" alt="Place Holder Book" style="width:150px; height:200px; margin-top: 20px" /><br>
+                                <img id="pic" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($blah['image']); ?>" alt="Place Holder Book" style="width:150px; height:200px; margin-top: 20px" /><br>
                             <?php } ?>
                         <?php } else { ?>
                             <p class="status error">Image(s) not found...</p> <?php } ?>
                         <!-- <img class="pic" src= "images/Gatsby.png" alt="Place Holder Book" style="width:150px;height:200px;"> -->
                         <h4 id="work"><?php echo $row->title; ?></h4>
                         <p id="auth"><?php echo $row->author; ?></p><br>
-                        <pre id="change">  Inventory: <?php echo $row->stock; ?>           <?php echo $row->price; ?></pre>
+                        <pre id="change">  Inventory: <?php echo $row->inventory; ?>           <?php echo $row->price; ?></pre>
                         <!-- <p id="change"><?php echo $row->price; ?></p><br> -->
-                        <button onclick="window.location.href='#'" class="EditText">Edit</button><br>
+                        <button onclick="window.location.href='admin-editBook.php'" class="EditText">Edit</button><br>
 
                     </div>
 
@@ -158,12 +160,7 @@
         }
     }
 		
-		else {
-			echo "Name Does not exist";
-		}
-
-
-    }
+		
                 
 
 ?>
